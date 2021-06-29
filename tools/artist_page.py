bb01153a9f0c3fd8d783f52bed8bfb55517755db
huyennm-1670 <maddie.nguyen0908@gmail.com>
2021-06-29 13:44:18 +0700

    fixed artist page, more secure method of connecting to sql

--- tools/artist_page.py
+++ tools/artist_page.py
@@ -1,7 +1,7 @@
 from google_spreadsheet_api.function import get_df_from_speadsheet, get_list_of_sheet_title, update_value, \
     creat_new_sheet_and_update_data_from_df, get_gsheet_name
 from google_spreadsheet_api.create_new_sheet_and_update_data_from_df import creat_new_sheet_and_update_data_from_df
-from Data_lake_process.class_definition import WhenExist, SheetNameType, DataReports, PageType
+from Data_lake_process.class_definition import WhenExist, DataReports, PageType
 
 from core.crud.get_df_from_query import get_df_from_query
 
@@ -10,8 +10,10 @@ import time
 from colorama import Fore, Style
 from core import query_path
 from core.crud.sql.query_supporter import count_datasource_by_artistname_formatid, get_datasource_by_artistname_formatid
-from Data_lake_process.data_lake_standard import update_data_reports, process_image, crawl_image_datalake, checking_image_crawler_status
+from Data_lake_process.data_lake_standard import update_data_reports
 from Data_lake_process.youtube_similarity import similarity
+from Data_lake_process.class_definition import Page
+from google_spreadsheet_api.function import get_list_of_sheet_title
 
 
 def check_youtube_url_mp3(gsheet_id: str):
@@ -483,9 +485,10 @@ if __name__ == "__main__":
     ]
     df = pd.DataFrame()
     for url in urls:
-        page_type = PageType.TopSingle(url=url)
-        sheet_info = SheetNameType.MP3_SHEET_NAME
-        df_ = page_type.media_file(sheet_info=sheet_info)
+        page_type = Page(url=url)
+        # print(get_list_of_sheet_title(page_type.gsheet_id))
+        sheet_info = page_type.sheet_name_type.MP3_SHEET_NAME
+        df_ = page_type.media_file(sheet_info=sheet_info, page_priority=2205)
         df = df.append(df_, ignore_index=True)
     print(df.head(10))
     print("\n --- total time to process %s seconds ---" % (time.time() - start_time))
