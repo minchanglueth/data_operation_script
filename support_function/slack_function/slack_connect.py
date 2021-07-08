042fb37099cf4b6f0a3d522efbd539656912c19f
Your Name <you@example.com>
2021-07-08 14:54:35 +0700

    edit slack function

--- support_function/slack_function/slack_connect.py
+++ support_function/slack_function/slack_connect.py
@@ -5,9 +5,10 @@ from slack_sdk import WebClient
 
 # Get application first: https://api.slack.com/apps/A02341UQV2M/oauth?success=1
 
-client_slack = WebClient(token='xoxb-1216139827667-1906835181267-xoLprkbWxRUG3Kv3xUVJ2iGO')  # slack ở bên M<3
-# client_slack = WebClient(token='xoxb-1216139827667-1906835181267-DJ8Y1MyqU1UlJxSVXBRYr6xI') # slack ở bên M<3
-# client_slack = WebClient(token='xoxb-3598555470-1887074714135-mtHVkOQia1xZXqI7QEt8yQWs') # slack ở VIBBIDI
+# client_slack = WebClient(token='xoxb-1216139827667-1906835181267-xoLprkbWxRUG3Kv3xUVJ2iGO')  # slack ở bên M<3
+# client_slack = WebClient(token='xoxb-1216139827667-1906835181267-DJ8Y1MyqU1UlJxSVXBRYr6xI') # slack ở bên M<3 cũ
+client_slack = WebClient(token='xoxb-3598555470-1887074714135-mOnbmqkIhyZUPwz0hxuGuiq2') # slack ở VIBBIDI
+# client_slack = WebClient(token='xoxb-3598555470-1887074714135-mtHVkOQia1xZXqI7QEt8yQWs') # slack ở VIBBIDI cũ
 # xoxb-3598555470-1887074714135-mtHVkOQia1xZXqI7QEt8yQWs
 # xapp-1-A01T7HUJ4F2-1887112284519-553554ddb5acb55a80238ea6b19e472f9a0ec9b9857a3032ee10cff784ba2dc8
 
