ae24060f36622ad56d478d30c30c5fda9dd7b3b8
Your Name <you@example.com>
2021-07-08 17:30:34 +0700

    update slack token

--- support_function/slack_function/slack_connect.py
+++ support_function/slack_function/slack_connect.py
@@ -6,11 +6,7 @@ from slack_sdk import WebClient
 # Get application first: https://api.slack.com/apps/A02341UQV2M/oauth?success=1
 
 # client_slack = WebClient(token='xoxb-1216139827667-1906835181267-xoLprkbWxRUG3Kv3xUVJ2iGO')  # slack ở bên M<3
-# client_slack = WebClient(token='xoxb-1216139827667-1906835181267-DJ8Y1MyqU1UlJxSVXBRYr6xI') # slack ở bên M<3 cũ
-client_slack = WebClient(token='xoxb-3598555470-1887074714135-mOnbmqkIhyZUPwz0hxuGuiq2') # slack ở VIBBIDI
-# client_slack = WebClient(token='xoxb-3598555470-1887074714135-mtHVkOQia1xZXqI7QEt8yQWs') # slack ở VIBBIDI cũ
-# xoxb-3598555470-1887074714135-mtHVkOQia1xZXqI7QEt8yQWs
-# xapp-1-A01T7HUJ4F2-1887112284519-553554ddb5acb55a80238ea6b19e472f9a0ec9b9857a3032ee10cff784ba2dc8
+client_slack = WebClient(token='xoxb-3598555470-1887074714135-OJVFqElZsRVY8YF2c0WVm8jW') # slack ở VIBBIDI
 
 # # để DELETE slack post
 # from slack_sdk.errors import SlackApiError
@@ -18,10 +14,10 @@ client_slack = WebClient(token='xoxb-3598555470-1887074714135-mOnbmqkIhyZUPwz0hx
 
 # try:
 #     # client_slack.chat_postMessage(channel='minchan-testing', text=str(report_crawler_updated)) # slack channel bên M<3
-#     # client_slack.chat_postMessage(channel='data-auto-report', text=str(report_crawler_updated))
-#     # client_slack.chat_postMessage(channel='data-internal', text=str(report_crawler_updated))
-#     client_slack.chat_delete(channel='C3C7S8KLP',ts='1620275236.101200')
-#     #client_slack.chat_delete(channel='C3C7S8KLP',ts='1618911613.083000')
+#     client_slack.chat_postMessage(channel='data-auto-report-error', text=str("testing"))
+# #     # client_slack.chat_postMessage(channel='data-internal', text=str(report_crawler_updated))
+# #     client_slack.chat_delete(channel='C3C7S8KLP',ts='1620275236.101200')
+# #     #client_slack.chat_delete(channel='C3C7S8KLP',ts='1618911613.083000')
 # except SlackApiError as e:
 # ## You will get a SlackApiError if "ok" is False
 #     assert e.response["ok"] is False
